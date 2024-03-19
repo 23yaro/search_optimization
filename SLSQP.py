28dbd9069924d405bebd87153d7792ca2daa819a
Alexander <147336344+23yaro@users.noreply.github.com>
2024-03-19 16:43:26 +0300

    2

---
+++ SLSQP.py
@@ -0,0 +1,44 @@
+from scipy.optimize import minimize
+import numpy as np
+import numpy
+
+
+def make_data_lab_2():
+    x = numpy.linspace(-10, 10, 100)
+    y = numpy.linspace(-10, 10, 100)
+
+    x_grid, y_grid = numpy.meshgrid(x, y)
+
+    z = 2 * x_grid * x_grid + 3 * y_grid * y_grid + 4 * x_grid * y_grid - 6 * x_grid - 3 * y_grid
+    return x_grid, y_grid, z
+
+
+def kp(x, y):
+    #   global points
+    points = []
+
+    def fun(x_i):  # Функция
+        x1 = x_i[0]
+        x2 = x_i[1]
+        return 2 * x1 * x1 + 3 * x2 * x2 + 4 * x1 * x2 - 6 * x1 - 3 * x2
+
+    def callback(x_w):
+        g_list = np.ndarray.tolist(x_w)
+        g_list.append(fun(x_w))
+        points.append(g_list)
+
+    b = (0, float("inf"))  # диапазон поиска
+    bounds = (b, b)
+    x0 = (x, y)  # начальная точка
+    con = {'type': 'eq', 'fun': fun}
+
+    # основной вызов
+    res = minimize(fun, x0, method="SLSQP", bounds=bounds,
+                   constraints=con, callback=callback)
+
+    glist = np.ndarray.tolist(res.x)
+    glist.append(res.fun)
+    points.append(glist)
+
+    for iteration, point in enumerate(points):
+        yield iteration, point
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tkinter.ttk import Notebook
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg)
 from Gradient import make_data_lab_1, funct_consider
+from SLSQP import make_data_lab_2, kp
 
 
 def main():
@@ -71,7 +72,7 @@ def main():
     main_f_tab_1 = LabelFrame(tab_1, text="Parameters")
     left_f_tab_1 = Frame(main_f_tab_1)
     right_f_tab_1 = Frame(main_f_tab_1)
-    txt_f_tab_1 = LabelFrame(tab_1, text="Выполнение и результаты")
+    txt_f_tab_1 = LabelFrame(tab_1, text="Result")
 
     lbl_1_tab_1 = Label(left_f_tab_1, text="X")
     lbl_2_tab_1 = Label(left_f_tab_1, text="Y")
@@ -96,8 +97,8 @@ def main():
     txt_5_tab_1.insert(0, "0.1")
 
     txt_tab_1 = scrolledtext.ScrolledText(txt_f_tab_1)
+    btn_del_tab_1 = Button(tab_1, text="Delete", command=delete_lab_1)
     btn_tab_1 = Button(tab_1, text="Start", foreground="black", command=draw_lab_1)
-    txt_f_tab_1.pack(side=BOTTOM, padx=5, pady=5, fill=BOTH, expand=True)
 
     lbl_5_tab_1.pack()
     main_f_tab_1.pack()
@@ -116,9 +117,94 @@ def main():
     txt_4_tab_1.pack()
     txt_5_tab_1.pack()
 
-    txt_tab_1.pack(padx=5, pady=5, fill=BOTH, expand=True)
-
     btn_tab_1.pack()
+    txt_tab_1.pack()
+    txt_f_tab_1.pack()
+    btn_del_tab_1.pack()
+
+    def draw_lab_2():
+        fig.clf()
+
+        x, y, z = make_data_lab_2()
+
+        res_x = txt_1_tab_2.get()
+        res_y = txt_2_tab_2.get()
+
+        ax = fig.add_subplot(projection='3d')
+        ax.plot_surface(x, y, z, rstride=5, cstride=5, alpha=0.5, cmap="inferno")
+        canvas.draw()
+
+        x_cs = []
+        y_cs = []
+        z_cs = []
+
+        for i, point in kp(res_x, res_y):
+            x_cs.append(point[0])
+            y_cs.append(point[1])
+            z_cs.append(point[2])
+
+        for i in range(len(x_cs)):
+            if i < (len(x_cs) - 1):
+                ax.scatter(x_cs[i - 1], y_cs[i - 1], z_cs[i - 1], c="black", s=1, marker="s")
+            else:
+                ax.scatter(x_cs[i - 1], y_cs[i - 1], z_cs[i - 1], c="red")
+
+            txt_tab_2.insert(INSERT, f"{i}) ({round(x_cs[i], 2)})({round(y_cs[i], 2)}) = {round(z_cs[i], 4)}\n")
+            canvas.draw()
+            ax.set_xlabel('X')
+            ax.set_ylabel('Y')
+            ax.set_zlabel('Z')
+            window.update()
+            delay = txt_3_tab_2.get()
+            time.sleep(float(delay))
+
+    def delete_lab_2():
+        txt_tab_2.delete(1.0, END)
+
+    tab_2 = Frame(tab_control)
+    tab_control.add(tab_2, text="LR2")
+
+    main_f_tab_2 = LabelFrame(tab_2, text="Parameters")
+    left_f_tab_2 = Frame(main_f_tab_2)
+    right_f_tab_2 = Frame(main_f_tab_2)
+    txt_f_tab_2 = LabelFrame(tab_2, text="Results")
+
+    lbl_1_tab_2 = Label(tab_2, text="Function :\n2 * x₁² + 3 * x₂² + 4 * x₁ * x₂ - 6 * x₁ - 3 * x₂")
+    lbl_2_tab_2 = Label(left_f_tab_2, text="X")
+    lbl_3_tab_2 = Label(left_f_tab_2, text="Y")
+    lbl_4_tab_2 = Label(left_f_tab_2, text="Delay")
+
+    txt_1_tab_2 = Entry(right_f_tab_2)
+    txt_1_tab_2.insert(0, "10")
+
+    txt_2_tab_2 = Entry(right_f_tab_2)
+    txt_2_tab_2.insert(0, "10")
+
+    txt_3_tab_2 = Entry(right_f_tab_2)
+    txt_3_tab_2.insert(0, "0.1")
+
+    txt_tab_2 = scrolledtext.ScrolledText(txt_f_tab_2)
+    btn_del_tab_2 = Button(tab_2, text="Delete", command=delete_lab_2)
+    btn_tab_2 = Button(tab_2, text="Start", command=draw_lab_2)
+
+    lbl_1_tab_2.pack()
+    main_f_tab_2.pack()
+    left_f_tab_2.pack(side=LEFT)
+    right_f_tab_2.pack(side=RIGHT)
+
+    lbl_2_tab_2.pack()
+    lbl_3_tab_2.pack()
+    lbl_4_tab_2.pack()
+
+    txt_1_tab_2.pack()
+    txt_2_tab_2.pack()
+    txt_3_tab_2.pack()
+
+    txt_tab_2.pack()
+
+    btn_tab_2.pack()
+    txt_f_tab_2.pack()
+    btn_del_tab_2.pack()
 
     tab_control.pack()
     window.mainloop()
